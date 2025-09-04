2ba2aa487aa30dec2721b25ec9c58edae1c38310
Kimhyogyeom <gyrua77@gmail.com>
2025-09-04 10:00:06 +0900

    feat: Add CameraFollowGround script, camera follow for moving Ground UI

---
+++ Assets/Scripts/Camera/CameraFollowGround.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollowGround : MonoBehaviour
+{
+    [SerializeField] private Transform _targetGround;
+    [SerializeField] private float _smoothTime = 0.3f;
+
+    private Vector3 _velocity = Vector3.zero;
+
+    void LateUpdate()
+    {
+        if (_targetGround == null) return;
+
+        Vector3 targetPos = new Vector3(transform.position.x, _targetGround.position.y - 1f, transform.position.z);
+
+        transform.position = Vector3.SmoothDamp(transform.position, targetPos, ref _velocity, _smoothTime);
+    }
+}
