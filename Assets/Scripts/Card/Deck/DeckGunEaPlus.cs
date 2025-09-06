1c8c34a7fc460209063472624ae57a81415c2f1b
Kimhyogyeom <gyrua77@gmail.com>
2025-09-06 17:38:03 +0900

    refactor: separate GunEaPlus logic into DeckGunEaPlus for modular skill handling

---
+++ Assets/Scripts/Card/Deck/DeckGunEaPlus.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeckGunEaPlus : MonoBehaviour
+{
+    [Header("Gun Settings")]
+    [SerializeField] private Transform _gunParentObject;
+    [SerializeField] private Transform _gunGenPos;
+    [SerializeField] private float _spacing = 0.2f;
+    [SerializeField] private GameObject[] _guns;
+
+    public void GunEaPlus(int level)
+    {
+        foreach (var gun in _guns)
+        {
+            print(gun.gameObject.name);
+            gun.SetActive(false);
+        }
+        int count = Mathf.Min(level + 1, _guns.Length);
+
+        float totalWidth = (count - 1) * _spacing;
+        float startX = -totalWidth / 2f;
+
+        for (int i = 0; i < _guns.Length; i++)
+        {
+            if (i < count)
+            {
+                _guns[i].SetActive(true);
+                Vector3 offset = new Vector3(startX + i * _spacing, 0f, 0f);
+                _guns[i].transform.position = _gunGenPos.position + offset;
+            }
+            else
+            {
+                _guns[i].SetActive(false);
+            }
+        }
+    }
+}
--- Assets/Scripts/Manager/SkillManager.cs
+++ Assets/Scripts/Manager/SkillManager.cs
@@ -2,36 +2,10 @@ using UnityEngine;
 
 public class SkillManager : MonoBehaviour
 {
-    [Header("Gun Settings")]
-    [SerializeField] private Transform _gunParentObject;
-    [SerializeField] private Transform _gunGenPos;
-    [SerializeField] private float _spacing = 0.2f;
-    [SerializeField] private GameObject[] _guns;
+    [SerializeField] private DeckGunEaPlus _deckGunEaPlus;
 
     public void GunEaPlus(int level)
     {
-        foreach (var gun in _guns)
-        {
-            print(gun.gameObject.name);
-            gun.SetActive(false);
-        }
-        int count = Mathf.Min(level + 1, _guns.Length);
-
-        float totalWidth = (count - 1) * _spacing;
-        float startX = -totalWidth / 2f;
-
-        for (int i = 0; i < _guns.Length; i++)
-        {
-            if (i < count)
-            {
-                _guns[i].SetActive(true);
-                Vector3 offset = new Vector3(startX + i * _spacing, 0f, 0f);
-                _guns[i].transform.position = _gunGenPos.position + offset;
-            }
-            else
-            {
-                _guns[i].SetActive(false);
-            }
-        }
+        _deckGunEaPlus.GunEaPlus(level);
     }
 }
